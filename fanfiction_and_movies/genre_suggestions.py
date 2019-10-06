0d438d5076336717ea6958da2c69d36b619a94c1
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-05 22:05:46 -0400

    After a first input, the code queries the database for an exact match

--- fanfiction_and_movies/genre_suggestions.py
+++ fanfiction_and_movies/genre_suggestions.py
@@ -7,5 +7,12 @@ print("Welcome to Jeeda and Naeem's film search program! Connecting to the film 
 conn= psycopg2.connect("dbname=movies user=nchowdhury")
 time.sleep(3)
 response= input ("Please enter the name of a film (case sensitive): ")
-while response== '':
-        response= input ("No input detected (case sensitive): ")
+while query== '':
+        query= input ("No input detected. Please remember how to type, and enter the name of a film (case sensitive): ")
+
+# Initiates the cursor object in psql
+cur= conn.cursor()
+cur.execute("select genre from movies where title='"+query+"';")
+# Captures the results of the query in an array of tuples.
+results = cur.fetchall()
+#print(results)
