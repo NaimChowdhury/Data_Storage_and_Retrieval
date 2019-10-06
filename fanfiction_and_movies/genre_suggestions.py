5e578a5ff3ca8db2440b4c85b03f6100c70e3523
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-05 22:01:54 -0400

    Added some whimsical introduction to the code, with a timer which feigns a wait. The exhilaration!

--- fanfiction_and_movies/genre_suggestions.py
+++ fanfiction_and_movies/genre_suggestions.py
@@ -1,2 +1,11 @@
 import sys
 import psycopg2
+
+
+print("Welcome to Jeeda and Naeem's film search program! Connecting to the film database.")
+# Connects to the movies database on the NCF server.
+conn= psycopg2.connect("dbname=movies user=nchowdhury")
+time.sleep(3)
+response= input ("Please enter the name of a film (case sensitive): ")
+while response== '':
+        response= input ("No input detected (case sensitive): ")
