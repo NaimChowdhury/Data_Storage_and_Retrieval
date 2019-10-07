109728fd240715d2872eda288c7897aa14119d16
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-07 13:26:37 -0400

    Added a for loop that prints the movies in a table-like format.

--- fanfiction_and_movies/fanfiction_search.py
+++ fanfiction_and_movies/fanfiction_search.py
@@ -24,6 +24,15 @@ cur = conn.cursor()
 
 cur.execute("SELECT title, description FROM stories WHERE title ~* '"+query+"' ORDER
 BY random() LIMIT 10;")
-results = cur.fetchall()
-print(results)
+# The resulting tuples of type (title, description) are stored in a list
+titletoops = cur.fetchall()
+
+print("The following randomized list of 10 or less fanfictions are those that contain the keywords in their title.")
+for i, d in enumerate(titletoops):
+        line = ' || '.join(str(x).ljust(12) for x in d)
+        print('-' * len(line))
+        print(line)
+        time.sleep(.5)
+
+print("Would you also like to see titles which description contain the keywords?")
 
