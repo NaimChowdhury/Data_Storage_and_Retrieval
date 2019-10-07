2bb6d5f9d7f344463e5f947a2cd61cbba64c5d4a
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-06 23:27:32 -0400

    Added some silly initial print statements.

--- fanfiction_and_movies/fanfiction_search.py
+++ fanfiction_and_movies/fanfiction_search.py
@@ -1,2 +1,11 @@
 import psycopg2
 import time
+
+conn= psycopg2.connect("dbname=fanfiction user=nchowdhury")
+
+print("Welcome to JnN Fanfiction Wonderland. What sort of trash are you into?")
+query = input("I'm looking for fanfiction related to: ")
+print("Thank you. One moment while we scour our databases for things related to %s.", query)
+
+cur = conn.cursor()
+
