bf522add3fa5b4c5bb6cb0f0466b9a0e3c8764d0
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-07 12:05:53 -0400

    Added a for loop that creates a regular expression for the search.

--- fanfiction_and_movies/fanfiction_search.py
+++ fanfiction_and_movies/fanfiction_search.py
@@ -1,11 +1,24 @@
 import psycopg2
 import time
 
+# Establish connection to database
 conn= psycopg2.connect("dbname=fanfiction user=nchowdhury")
 
-print("Welcome to JnN Fanfiction Wonderland. What sort of trash are you into?")
+print("Welcome to JnN Fanfiction Wonderland. What sort of trash are you into? Please do not include apostrophes, articles, or conjuctions.")
 query = input("I'm looking for fanfiction related to: ")
 print("Thank you. One moment while we scour our databases for things related to %s.", query)
+
+# In order to prepare the string for regular expressions, a for loop will prepare the expression
+query = query.split()
+string = '('
+for i in range(len(query)):
+    if i == len(query)-1:
+        string += query[i] + ')'
+    else:
+        string += query[i] + '|'
+
+query = string
+print(query)
 
 cur = conn.cursor()
 
