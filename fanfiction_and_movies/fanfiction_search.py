138665f0fdb7a93f646d17bbe257e15da226fe04
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-08 21:45:56 -0400

    Finalized the portion which asks user whether or not they'd like to search descriptions for their key words.

--- fanfiction_and_movies/fanfiction_search.py
+++ fanfiction_and_movies/fanfiction_search.py
@@ -36,3 +36,18 @@ for i, d in enumerate(titletoops):
 
 print("Would you also like to see titles which description contain the keywords?")
 
+query2= input("y/n: ")
+
+if query2== "y":
+        cur.execute("SELECT title, description FROM stories WHERE description ~* '"+query+"' ORDER BY random() LIMIT 10;")
+# The resulting tuples of type (title, description) are stored in a list
+        titletoops = cur.fetchall()
+        print("The following randomized list of 10 or less fanfictions are those that contain the keywords in their description.")
+        for i, d in enumerate(titletoops):
+                line = ' || '.join(str(x).ljust(12) for x in d)
+                print('-' * len(line))
+                print(line)
+                time.sleep(.5)
+if query2== "n":
+        print("Alright then. Thanks for visiting!")
+
